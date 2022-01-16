196c2d17b124ef227700ce46d9ee0329ddea0798
bruno-kwemoi <bsaekwo@gmail.com>
2022-01-16 03:05:26 +0300

    betty

--- sort.h
+++ sort.h
@@ -4,7 +4,7 @@
 /* void print_list(const listint_t *list); */
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-  
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,9 +14,9 @@ void bubble_sort(int *array, size_t size);
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 #endif
