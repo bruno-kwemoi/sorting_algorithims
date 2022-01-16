9d54db73e9c1d1db2e33af4773f746dab5e72d63
bruno-kwemoi <bsaekwo@gmail.com>
2022-01-16 18:03:00 +0300

    corrected output

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,9 +26,12 @@ void selection_sort(int *array, size_t size)
 		min_idx = i;
 
 		for (j = i + 1; j < n; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
+		}
 		swap(&array[min_idx], &array[i]);
-		print_array(array, n);
+		if (i != min_idx)
+			print_array(array, n);
 	}
 }
